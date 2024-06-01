b39f76c29cb5499ee355bcd9171471e46a6c00fd
Pham Quang Thai <87228468+bug1422@users.noreply.github.com>
2024-06-01 15:49:29 +0700

    Commit

---
+++ scenes/main_scene.py
@@ -0,0 +1,25 @@
+from model import *
+import glm
+
+class Scene:
+    def __init__(self, app):
+        self.app = app
+        self.objects = []
+        self.load()
+        # skybox
+        self.skybox = AdvancedSkyBox(app)
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def load(self):
+        app = self.app
+        add = self.add_object
+        # cat
+        add(Cat(app, pos=(0, -1, -10)))
+
+        # menu
+        add()
+
+    def update(self):
+        self.moving_cube.rot.xyz = self.app.time
